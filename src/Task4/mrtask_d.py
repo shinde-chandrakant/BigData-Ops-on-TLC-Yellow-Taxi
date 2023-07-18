7845e01b7fd94d559f13938429d5c8141931b765
chandrakant shinde <chandrakantshind3@gmail.com>
2023-07-18 11:19:40 +0530

    add Task_helper.txt, mod of mrtask_d.py

--- src/Task4/mrtask_d.py
+++ src/Task4/mrtask_d.py
@@ -1,15 +1,27 @@
 # What is the average trip time for different pickup locations?
 
 from mrjob.job import MRJob
+from datetime import datetime
 
 class AverageTripTime(MRJob):
+
+    def parse_datetime(self, datetime_str):
+        formats = ['%d-%m-%Y %H:%M:%S', '%d-%m-%Y %H:%M', '%Y-%m-%d %H:%M', '%Y-%m-%d %H:%M:%S']
+        for fmt in formats:
+            try:
+                return datetime.strptime(datetime_str, fmt)
+            except ValueError:
+                pass
+        raise ValueError('no valid date format found')
 
     def mapper(self, _, line):
         # Skip the header line
         if not line.startswith('VendorID'):
             fields = line.split(',')
             pickup_location = fields[7]
-            trip_time = float(fields[4])
+            pickup_datetime = self.parse_datetime(fields[1])
+            dropoff_datetime = self.parse_datetime(fields[2])
+            trip_time = (dropoff_datetime - pickup_datetime).total_seconds() / 60.0
             yield pickup_location, (trip_time, 1)
 
     def combiner(self, pickup_location, trip_times):
